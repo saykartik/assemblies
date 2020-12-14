dd381df2dbfc9146939f1fbf4156ce99ba76f89b
Kartik Balasubramaniam <saykartik@gmail.com>
2020-12-13 23:42:47 -0800

    Update adversarial comparisons and saved images.

--- BrainNet/AdversarialExamples.py
+++ BrainNet/AdversarialExamples.py
@@ -17,12 +17,17 @@ import matplotlib.pyplot as plt
 #     if init_pred.item() != y.item():
 #         return inp
 
-def adversarial_example(x, y, net, eps=100, lr=1e-2, verbose=False,show_grad=False):
+def adversarial_example(x, y, net, eps=100, lr=1e-2, verbose=False, show_grad=False, full_net=None):
     # inp = nn.Parameter(x + torch.randn_like(x) / 50)
+    if eps <= 0.00001:
+        return x
+
     inp = nn.Parameter(x)
+
     original = x.clone()
 
     optimizer = optim.Adam([inp], lr=lr)
+    # optimizer = optim.Adam(full_net.parameters(), lr=lr)
 
     criterion = nn.CrossEntropyLoss()
 
@@ -39,7 +44,7 @@ def adversarial_example(x, y, net, eps=100, lr=1e-2, verbose=False,show_grad=Fal
 
         if np.linalg.norm(inp.data - original.data) > eps: break
         outputs = net(inp)
-        #print("o: ",outputs, "target: ", y)
+
         loss = criterion(outputs, y)
         #         print("output:", outputs.data, y.data)
         if verbose:
@@ -49,10 +54,14 @@ def adversarial_example(x, y, net, eps=100, lr=1e-2, verbose=False,show_grad=Fal
 
         loss.backward()
         if show_grad and cnt == 1:
+            print("Input Gradients")
             print(inp.grad)
+            print("All Gradients")
+            print(full_net.input_weights.grad)
+            print(full_net.output_weights.grad)
 
         if cnt == 100:
-            print("too many steps to create adversary. Exiting.")
+            # print("too many steps to create adversary. Exiting.")
             break
         optimizer.step()
 
@@ -66,7 +75,7 @@ def random_not_true_label(label):
     return choice
 
 
-def get_adv_bulk_data(data, model, eps,limit=None):
+def get_adv_bulk_data(data, model, eps, limit=None):
     """
     :param data: a list of tuples of (X,Y) observations
     :param model: any NN model which is supported in this framework
@@ -75,7 +84,7 @@ def get_adv_bulk_data(data, model, eps,limit=None):
     """
     new_X = []
     new_Y = []
-    ctr=1
+    ctr = 1
     for point in data:
         X, Y = point
         # Choose a random Y which is not the current Y
@@ -95,43 +104,69 @@ def get_adv_bulk_data(data, model, eps,limit=None):
     return np.array(new_X), np.array(new_Y)
 
 
-def epsilon_sensitivity_trend(models, orig_test_data, num_labels,limit=None,verbose=False):
+def epsilon_sensitivity_trend(
+        models,
+        orig_test_data,
+        num_labels,
+        limit=None,
+        verbose=False,
+        models_agree=True,
+        gd_adversary_model='Full GD'
+):
     """
     :param num_labels:
     :param models: A dict of models with keys as names and values as trained NN models
     :param orig_test_data: A list tuples of (X,Y) observations
+    :param limit: Limit the number of datapoints of adversarial examples
+    :param verbose: Talk a lot.
+    :param models_agree - All models must agree on the output for test example.Useful only for high accuracy models
+    :param gd_adversary_only: Adversaries are generated from the Full GD model only
     :return: A dict of timeseries of accuracies varying with epsilon pertrubation
     """
 
-    epsilon_performance = {}
     epsilons = np.arange(0, 4, 0.5)  # increment epsilon in range[0,6] in steps of 0.3
 
     # Find all examples where models agree on the output label on the raw data
     def models_agree(X, Y, mods):
-        all_models_agree = True
         for _, m in mods.items():
-            pred_Y = m.forward_pass(X.reshape(1, -1)).argmax()  # Fix the call, depending on which framework is used.
-            if pred_Y.item() != Y:
-                all_models_agree = False
-                break
+            with torch.no_grad():
+                pred_Y = m.forward_pass(X.reshape(1, -1)).argmax(axis=1).numpy()
 
-        return all_models_agree
+                if pred_Y != Y:
+                    return False
+
+        return True
 
     orig_test_data = [(torch.from_numpy(X).double(), Y) for X, Y in orig_test_data]
-    new_data = [(X, Y) for X, Y in orig_test_data if models_agree(X, Y, models)]
-    print(len(new_data))
-    # new_data = orig_test_data
-    for model_name, model in models.items():
-        accuracy_trend = []
-        print("Model: ", model_name)
-        for epsilon in epsilons:
-            this_model = model
-            X, Y = get_adv_bulk_data(new_data, this_model.forward_pass, epsilon,limit=limit)
-            acc, _ = evaluate(X, Y, num_labels, this_model.forward_pass,verbose=verbose)
-            accuracy_trend.append(acc)
-            print("**************** EPS: ", epsilon)
-            print("ACC: ", acc)
 
-        epsilon_performance[model_name] = accuracy_trend
+    if models_agree:
+        new_data = [(X, Y) for X, Y in orig_test_data if models_agree(X, Y, models)]
+    else:
+        new_data = orig_test_data
+
+    print("Base Test Data Size: ", len(new_data))
+    epsilon_performance = {}
+    if len(gd_adversary_model):
+        full_gd_model = models[gd_adversary_model]
+        for model_name, model in models.items():
+            epsilon_performance[model_name] = []
+        for epsilon in epsilons:
+            X, Y = get_adv_bulk_data(new_data, full_gd_model.forward_pass, epsilon, limit=limit)
+            for model_name, model in models.items():
+                acc, _ = evaluate(X, Y, num_labels, model.forward_pass, verbose=verbose, ignore_missed_class=True)
+                print("Model,EPS,Acc", model_name, epsilon, acc)
+                epsilon_performance[model_name].append(acc)
+    else:
+        for model_name, model in models.items():
+            accuracy_trend = []
+            print("Model: ", model_name)
+            for epsilon in epsilons:
+                this_model = model
+                X, Y = get_adv_bulk_data(new_data, this_model.forward_pass, epsilon, limit=limit)
+                acc, _ = evaluate(X, Y, num_labels, this_model.forward_pass, verbose=verbose, ignore_missed_class=True)
+                accuracy_trend.append(acc)
+                print("Model,EPS,Acc", model_name, epsilon, acc)
+
+            epsilon_performance[model_name] = accuracy_trend
 
     return epsilon_performance, epsilons
